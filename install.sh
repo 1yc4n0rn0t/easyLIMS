72242a46fdec2c10a727defd49b32b47470cce50
Lycan <168678656+1yc4n0rn0t@users.noreply.github.com>
2024-05-08 07:44:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
+```
+                       _     ___ __  __ ____  
+   ___  __ _ ___ _   _| |   |_ _|  \/  / ___| 
+  / _ \/ _` / __| | | | |    | || |\/| \___ \ 
+ |  __/ (_| \__ \ |_| | |___ | || |  | |___) |
+  \___|\__,_|___/\__, |_____|___|_|  |_|____/ 
+                 |___/                        
+
+```
+
 # Start Python HTTP server
-python3 -m http.server
+python -m http.server 8000 &
+
+# Get the PID of the Python HTTP server process
+PID=$!
+
+# Print out the link to localhost on port 8000
+echo "Server running at http://localhost:8000"
+
+# Wait for the user to stop the server
+read -p "Press any key to stop the server..."
+
+# Stop the Python HTTP server
+kill $PID
